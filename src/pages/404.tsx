b1c8a81761f1cc62a9deedbe0c631bb4d8934b20
Carlo <hola.carlodominguez@gmail.com>
2022-09-25 23:21:35 +0200

    feat: adds custom error pages

---
+++ src/pages/404.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next"
+
+const Page: NextPage = () => {
+  return (
+    <main className="min-h-screen p-8 flex items-center justify-center">
+      <h1 className="text-6xl font-bold text-center">404</h1>
+    </main>
+  )
+}
+
+export default Page
