e42510e8ab9c865c0464d9d458ff9c65c04d2a7a
Carlo <hola.carlodominguez@gmail.com>
2022-09-26 17:56:43 +0200

    chore: adds some names

--- src/data/items.ts
+++ src/data/items.ts
@@ -35,4 +35,195 @@ export const items: Item[] = [
   {
     name: "Locura",
   },
+  {
+    name: "Espina",
+    tags: [tags.cat, tags.cockatiel],
+  },
+  {
+    name: "Hoja",
+    tags: [tags.cat, tags.cockatiel],
+  },
+  {
+    name: "Dardo",
+    tags: [tags.cat, tags.cockatiel, tags.ferret],
+  },
+  {
+    name: "Vena",
+    tags: [tags.cat, tags.cockatiel],
+  },
+  {
+    name: "Napoleón",
+    tags: [tags.cat, tags.cockatiel],
+  },
+  {
+    name: "Picote",
+    tags: [tags.cockatiel],
+  },
+  {
+    name: "Tenzin",
+  },
+  {
+    name: "Respeto",
+  },
+  {
+    name: "Popicó",
+    tags: [tags.cockatiel],
+  },
+  {
+    name: "Ñañil",
+    tags: [tags.cockatiel],
+  },
+  {
+    name: "Alma",
+    tags: [tags.cat, tags.cockatiel, tags.ferret],
+  },
+  {
+    name: "Tubo",
+    tags: [tags.ferret],
+  },
+  {
+    name: "Araña",
+  },
+  {
+    name: "Mono araña",
+  },
+  {
+    name: "Destructor",
+  },
+  {
+    name: "Ácido hialurónico",
+  },
+  {
+    name: "Cerilla",
+    tags: [tags.cat, tags.cockatiel, tags.ferret],
+  },
+  {
+    name: "Desconocido",
+  },
+  {
+    name: "Usuario",
+  },
+  {
+    name: "Fiera",
+  },
+  {
+    name: "Chorro",
+  },
+  {
+    name: "Ketchup",
+    tags: [tags.cat, tags.cockatiel],
+  },
+  {
+    name: "King de los prados",
+    tags: [tags.dog],
+  },
+  {
+    name: "Monedero",
+  },
+  {
+    name: "Cisterna",
+  },
+  {
+    name: "Chipollo",
+    tags: [tags.cat, tags.cockatiel],
+  },
+  {
+    name: "Espada",
+    tags: [tags.cat, tags.cockatiel, tags.ferret],
+  },
+  {
+    name: "Dirección",
+  },
+  {
+    name: "Basmati",
+    tags: [tags.cat],
+  },
+  {
+    name: "Coseno",
+  },
+  {
+    name: "Antorcha",
+  },
+  {
+    name: "Tubo seminífero",
+  },
+  {
+    name: "Sonidos de pedos",
+  },
+  {
+    name: "Pulmón ",
+  },
+  {
+    name: "Gormiti",
+  },
+  {
+    name: "jenucha",
+  },
+  {
+    name: "Rojo vivo",
+  },
+  {
+    name: "Apiretal",
+  },
+  {
+    name: "Chicago",
+    tags: [tags.cockatiel],
+  },
+  {
+    name: "Guinda",
+  },
+  {
+    name: "Penique",
+  },
+  {
+    name: "Cocatino",
+    tags: [tags.cockatiel],
+  },
+  {
+    name: "Popileno",
+    tags: [tags.cockatiel],
+  },
+  {
+    name: "Mochegue",
+    tags: [tags.dog, tags.cat],
+  },
+  {
+    name: "Cuchillo",
+    tags: [tags.cat, tags.cockatiel, tags.ferret],
+  },
+  {
+    name: "Gogita",
+    tags: [tags.cockatiel],
+  },
+  {
+    name: "Almíbar",
+  },
+  {
+    name: "Loncha",
+  },
+  {
+    name: "Gerber",
+    tags: [tags.cat],
+  },
+  {
+    name: "Fispe",
+    tags: [tags.cockatiel],
+  },
+  {
+    name: "Diorite",
+  },
+  {
+    name: "Tumba",
+  },
+  {
+    name: "Garibolo",
+  },
+  {
+    name: "Poide",
+    tags: [tags.cat, tags.ferret],
+  },
+  {
+    name: "Finche",
+    tags: [tags.cockatiel],
+  },
 ]
