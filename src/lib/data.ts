4229351fe3278613457cc4497c5521ebb9601558
Carlo <hola.carlodominguez@gmail.com>
2023-10-18 12:44:04 +0200

    removes mocks

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,7 +1,5 @@
 import { Client } from "@notionhq/client"
 import { colors } from "tailwind.config"
-import datagetitems from "./data-getItems.json"
-import datagettags from "./data-getTags.json"
 
 const DATABASE_ID = "57dc8a63a9684fd89a368a6e3c7a08c6"
 
@@ -40,26 +38,25 @@ function getColor(color: string) {
 }
 
 export async function getTags() {
-  // const database = await notion.databases.retrieve({
-  //   database_id: DATABASE_ID,
-  // })
+  const database = await notion.databases.retrieve({
+    database_id: DATABASE_ID,
+  })
 
-  // if (!("Kind" in database.properties)) {
-  //   throw Error('Missing property "Kind" in Notion database.')
-  // }
+  if (!("Kind" in database.properties)) {
+    throw Error('Missing property "Kind" in Notion database.')
+  }
 
-  // if (database.properties.Kind.type !== "multi_select") {
-  //   throw Error('Invalid type of "Kind" property in Notion database.')
-  // }
+  if (database.properties.Kind.type !== "multi_select") {
+    throw Error('Invalid type of "Kind" property in Notion database.')
+  }
 
-  // return database.properties.Kind.multi_select.options
-  //   .map((option) => ({
-  //     name: option.name,
-  //     slug: slugify(option.name),
-  //     color: getColor(option.color),
-  //   }))
-  //   .sort((a, b) => collator.compare(a.slug, b.slug))
-  return datagettags
+  return database.properties.Kind.multi_select.options
+    .map((option) => ({
+      name: option.name,
+      slug: slugify(option.name),
+      color: getColor(option.color),
+    }))
+    .sort((a, b) => collator.compare(a.slug, b.slug))
 }
 
 export type Tag = Awaited<ReturnType<typeof getTags>>[number]
@@ -71,65 +68,66 @@ export type Item = {
 }
 
 export async function getItems() {
-  // let data = await notion.databases.query({
-  //   database_id: DATABASE_ID,
-  // })
+  let data = await notion.databases.query({
+    database_id: DATABASE_ID,
+  })
 
-  // let results = [...data.results]
+  let results = [...data.results]
 
-  // while (data.has_more && data.next_cursor) {
-  //   data = await notion.databases.query({
-  //     database_id: DATABASE_ID,
-  //     start_cursor: data.next_cursor,
-  //   })
+  while (data.has_more && data.next_cursor) {
+    data = await notion.databases.query({
+      database_id: DATABASE_ID,
+      start_cursor: data.next_cursor,
+    })
 
-  //   results = [...results, ...data.results]
-  // }
+    results = [...results, ...data.results]
+  }
 
-  // const tags = await getTags()
-  // const tagsMap = new Map<string, Tag>(tags.map((tag) => [tag.name, tag]))
+  const tags = await getTags()
+  const tagsMap = new Map<string, Tag>(tags.map((tag) => [tag.name, tag]))
 
-  // const processedResults = results.reduce((accumulator, result) => {
-  //   if (!("properties" in result)) return accumulator
-  //   if (!("Name" in result.properties)) return accumulator
-  //   if (!("title" in result.properties.Name)) return accumulator
-  //   if (!result.properties.Name.title.length) return accumulator
+  const processedResults = results.reduce((accumulator, result) => {
+    if (!("properties" in result)) return accumulator
+    if (!("Name" in result.properties)) return accumulator
+    if (!("title" in result.properties.Name)) return accumulator
+    if (!result.properties.Name.title.length) return accumulator
 
-  //   const name = capitalize(
-  //     result.properties.Name.title.map((title) => title.plain_text).join(" ")
-  //   )
+    const name = capitalize(
+      result.properties.Name.title.map((title) => title.plain_text).join(" "),
+    )
 
-  //   const slug = slugify(name)
+    const slug = slugify(name)
 
-  //   if (accumulator.has(slug)) {
-  //     console.warn(`Skipping duplicated item: ${name}`)
-  //     return accumulator
-  //   }
+    if (accumulator.has(slug)) {
+      console.warn(`Skipping duplicated item: ${name}`)
+      return accumulator
+    }
 
-  //   let resultTags: Tag[] = []
+    let resultTags: Tag[] = []
 
-  //   if ("multi_select" in result.properties.Kind) {
-  //     resultTags = result.properties.Kind.multi_select
-  //       .reduce<typeof resultTags>((accumulator, value) => {
-  //         const tag = tagsMap.get(value.name)
-  //         if (tag) accumulator.push(tag)
-  //         return accumulator
-  //       }, [])
-  //       .sort((a, b) => collator.compare(a.slug, b.slug))
-  //   }
+    if (
+      "multi_select" in result.properties.Kind! &&
+      Array.isArray(result.properties.Kind.multi_select)
+    ) {
+      resultTags = result.properties.Kind.multi_select
+        .reduce<typeof resultTags>((accumulator, value) => {
+          const tag = tagsMap.get(value.name)
+          if (tag) accumulator.push(tag)
+          return accumulator
+        }, [])
+        .sort((a, b) => collator.compare(a.slug, b.slug))
+    }
 
-  //   return accumulator.set(slug, {
-  //     name,
-  //     slug,
-  //     tags: resultTags,
-  //   })
-  // }, new Map<string, Item>())
+    return accumulator.set(slug, {
+      name,
+      slug,
+      tags: resultTags,
+    })
+  }, new Map<string, Item>())
 
-  // return Array.from(processedResults.values()).sort((a, b) =>
-  //   a.name.localeCompare(b.name)
-  // )
-
-  return datagetitems
+  return Array.from(processedResults.values()).sort((a, b) =>
+    a.name.localeCompare(b.name),
+  )
 }
 
 export async function getItemsByTagSlug(slug: string) {
